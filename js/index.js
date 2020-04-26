d39c6b68933827e0eaceb1c218da2f34432c8683
Andres <xxdrews@gmail.com>
2020-04-26 13:04:19 +0200

    extra weather info

--- js/index.js
+++ js/index.js
@@ -1,22 +1,11 @@
 $(document).ready(function () {
-    console.log("Jquery y la web cargados");
+    console.log("Jquery working");
     $("#city").keypress(function (e) {
+        //enter as submit button
         if (e.which == 13) {
             searchCity();
         }
     });
-    // var theCity = $("#city").val();
-    // alert()
-    // var key = "439d4b804bc8187953eb36d2a8c26a02";
-    //https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02
-    // $.getJSON("https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/weather?q=" + theCity + ",uk&appid=" + key ,function(response){
-    //     console.log(response);
-    //     console.log(response.weather[0].main);
-    //     $("#datos").append("<p>Clouds: " + response.base +"</p>");
-
-    // $("#datos").append("<p>" +"Clouds: " + response.base + "</p>");
-
-    //     });
 });
 
 function searchCity() {
@@ -26,12 +15,13 @@ function searchCity() {
         console.log(response);
         console.log(response.weather[0].main);
         var newFlag = "https://www.countryflags.io/" + response.sys.country + "/flat/64.png";
-        // console.log(response.sys.country);
-        // $("#datos").append("<img src='https://www.countryflags.io/' + 'GB' + '/flat/64.png'>");
-        $("#datos").append("<h3>Your selected city is " + response.name +"</h3>");
+        $("#datos").append("<h3>You have selected " + response.name +"</h3>");
         $("#datos").append('<img src="https://www.countryflags.io/' + response.sys.country + '/flat/64.png">');
 
-        $("#datos").append("<p>The temperature right now is " + Math.round(response.main.temp-273.15) +"°C" +"</p>");
+        $("#datos").append("<p>The temperature is " + Math.round(response.main.temp-273.15) +"°C" +"</p>");
+        $("#datos").append("<p>The humidity is " + Math.round(response.main.humidity) +"%" +"</p>");
+
+        $("#datos").append("<p>The wind is " + Math.round(response.wind.speed) +" km/h" +"</p>");
 
 
         document.getElementById("city").value = "";
